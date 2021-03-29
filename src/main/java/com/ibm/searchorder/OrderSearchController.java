592a4b54398c226ddabaa758282e93445cbd766d
unknown <ibmjavanh02@iiht.tech>
2021-03-29 12:57:13 +0530

    only doing search order seperately

---
+++ src/main/java/com/ibm/searchorder/OrderSearchController.java
@@ -0,0 +1,31 @@
+package com.ibm.searchorder;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ibm.searchorder.OrderService;
+import com.ibm.searchorder.Order;
+
+@RestController
+public class OrderSearchController {
+	@Autowired //is used for DI
+	OrderService orderService;   // DI
+	Order order;
+	
+	
+	@GetMapping("/order")
+	List<Order> getOrders() {
+		return orderService.getOrders();
+	}
+	@GetMapping("/order/{id}")
+	Optional<Order> getOrder(@PathVariable ("id") String orderId) {
+		return orderService.getOrder(orderId);
+	}
+
+}
